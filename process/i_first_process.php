fed3df98fbe30cb22a2bb084b5dc850664ea6bb9
Sen-Sen19 <marcneilsen021900@gmail.com>
2024-09-13 16:40:32 +0800

    Done database

--- process/i_first_process.php
+++ process/i_first_process.php
@@ -4,7 +4,6 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 include 'conn.php';
 
-
 $input = file_get_contents('php://input');
 $data = json_decode($input, true);
 
@@ -22,10 +21,8 @@ if (isset($data['data']) && !empty($data['data'])) {
         return array_filter($row);
     });
 
-
-
-
     foreach ($rows as $row) {
+
         $base_product = isset($row[0]) ? trim($row[0]) : '';
         $car_model = isset($row[1]) ? trim($row[1]) : '';
         $product = isset($row[2]) ? trim($row[2]) : '';
@@ -75,251 +72,272 @@ if (isset($data['data']) && !empty($data['data'])) {
         $nsc_10 = isset($row[46]) ? trim($row[46]) : '';
 
         $sql = "
-       IF EXISTS (
-    SELECT 1 
-    FROM first_process 
-    WHERE car_model = ? 
-      AND product = ? 
-      AND car_code = ? 
-      AND block = ? 
-      AND class = ?
-)
-BEGIN
-    UPDATE first_process
-    SET base_product = ?, 
-        line_no = ?, 
-        circuit_qty = ?, 
-        trd_nwpa_0_13 = ?,
-        trd_nwpa_below_2_0_except_0_13 = ?, 
-        trd_nwpa_2_0_3_0 = ?,
-        trd_wpa_0_13 = ?,
-        trd_wpa_below_2_0_except_0_13 = ?,
-        trd_wpa_2_0_3_0 = ?,
-        tr327 = ?,
-        tr328 = ?,
-        trd_aluminum_nwpa_2_0 = ?,
-        trd_aluminum_nwpa_below_2_0 = ?,
-        trd_aluminum_wpa_2_0 = ?,
-        trd_aluminum_wpa_below_2_0 = ?,
-        aluminum_dimension_check_aluminum_terminal_inspection = ?,
-        aluminum_visual_inspection = ?,
-        aluminum_coating_uv_ii = ?,
-        aluminum_image_inspection = ?,
-        aluminum_uv_iii = ?,
-        trd_alpha_aluminum_nwpa = ?,
-        trd_alpha_aluminum_wpa = ?,
-        aluminum_visual_inspection_for_alpha = ?,
-        enlarged_terminal_check_for_alpha = ?,
-        air_water_leak_test = ?,
-        sam_sub_no_airbag = ?,
-        sam_sub_no_normal = ?,
-        jam_auto_crimping_and_twisting = ?,
-        trd_alpha_aluminum_5_0_above = ?,
-        point_marking_nsc = ?,
-        point_marking_sam = ?,
-        enlarged_terminal_check_aluminum = ?,
-        nsc_1 = ?,
-        nsc_2 = ?,
-        nsc_3 = ?,
-        nsc_4 = ?,
-        nsc_5 = ?,
-        nsc_6 = ?,
-        nsc_7 = ?,
-        nsc_8 = ?,
-        nsc_9 = ?,
-        nsc_10 = ?
+        MERGE first_process AS target
+        USING (VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?,
+                       ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,
+                       ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,
+                       ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,
+                       ?, ?, ?, ?, ?, ?, ?))
+        AS source (
+base_product, 
+car_model, 
+product, 
+car_code, 
+block, 
+class, 
+line_no, 
+circuit_qty, 
+trd_nwpa_0_13, 
+trd_nwpa_below_2_0_except_0_13, 
+trd_nwpa_2_0_3_0, 
+trd_wpa_0_13, 
+trd_wpa_below_2_0_except_0_13, 
+trd_wpa_2_0_3_0, 
+tr327, 
+tr328, 
+trd_aluminum_nwpa_2_0, 
+trd_aluminum_nwpa_below_2_0, 
+trd_aluminum_wpa_2_0, 
+trd_aluminum_wpa_below_2_0, 
+aluminum_dimension_check_aluminum_terminal_inspection, 
+aluminum_visual_inspection, 
+aluminum_coating_uv_ii, 
+aluminum_image_inspection, 
+aluminum_uv_iii, 
+trd_alpha_aluminum_nwpa, 
+trd_alpha_aluminum_wpa, 
+aluminum_visual_inspection_for_alpha, 
+enlarged_terminal_check_for_alpha, 
+air_water_leak_test, 
+sam_sub_no_airbag, 
+sam_sub_no_normal, 
+jam_auto_crimping_and_twisting, 
+trd_alpha_aluminum_5_0_above, 
+point_marking_nsc, 
+point_marking_sam, 
+enlarged_terminal_check_aluminum, 
+nsc_1, 
+nsc_2, 
+nsc_3, 
+nsc_4, 
+nsc_5, 
+nsc_6, 
+nsc_7, 
+nsc_8, 
+nsc_9, 
+nsc_10)
 
+  ON target.car_model = source.car_model 
+           AND target.product = source.product 
+           AND target.car_code = source.car_code 
+           AND target.block = source.block 
+           AND target.class = source.class
+        WHEN MATCHED THEN
+            UPDATE SET 
+ target.base_product = source.base_product,
+target.car_model = source.car_model,
+target.product = source.product,
+target.car_code = source.car_code,
+target.block = source.block,
+target.class = source.class,
+target.line_no = source.line_no,
+target.circuit_qty = source.circuit_qty,
+target.trd_nwpa_0_13 = source.trd_nwpa_0_13,
+target.trd_nwpa_below_2_0_except_0_13 = source.trd_nwpa_below_2_0_except_0_13,
+target.trd_nwpa_2_0_3_0 = source.trd_nwpa_2_0_3_0,
+target.trd_wpa_0_13 = source.trd_wpa_0_13,
+target.trd_wpa_below_2_0_except_0_13 = source.trd_wpa_below_2_0_except_0_13,
+target.trd_wpa_2_0_3_0 = source.trd_wpa_2_0_3_0,
+target.tr327 = source.tr327,
+target.tr328 = source.tr328,
+target.trd_aluminum_nwpa_2_0 = source.trd_aluminum_nwpa_2_0,
+target.trd_aluminum_nwpa_below_2_0 = source.trd_aluminum_nwpa_below_2_0,
+target.trd_aluminum_wpa_2_0 = source.trd_aluminum_wpa_2_0,
+target.trd_aluminum_wpa_below_2_0 = source.trd_aluminum_wpa_below_2_0,
+target.aluminum_dimension_check_aluminum_terminal_inspection = source.aluminum_dimension_check_aluminum_terminal_inspection,
+target.aluminum_visual_inspection = source.aluminum_visual_inspection,
+target.aluminum_coating_uv_ii = source.aluminum_coating_uv_ii,
+target.aluminum_image_inspection = source.aluminum_image_inspection,
+target.aluminum_uv_iii = source.aluminum_uv_iii,
+target.trd_alpha_aluminum_nwpa = source.trd_alpha_aluminum_nwpa,
+target.trd_alpha_aluminum_wpa = source.trd_alpha_aluminum_wpa,
+target.aluminum_visual_inspection_for_alpha = source.aluminum_visual_inspection_for_alpha,
+target.enlarged_terminal_check_for_alpha = source.enlarged_terminal_check_for_alpha,
+target.air_water_leak_test = source.air_water_leak_test,
+target.sam_sub_no_airbag = source.sam_sub_no_airbag,
+target.sam_sub_no_normal = source.sam_sub_no_normal,
+target.jam_auto_crimping_and_twisting = source.jam_auto_crimping_and_twisting,
+target.trd_alpha_aluminum_5_0_above = source.trd_alpha_aluminum_5_0_above,
+target.point_marking_nsc = source.point_marking_nsc,
+target.point_marking_sam = source.point_marking_sam,
+target.enlarged_terminal_check_aluminum = source.enlarged_terminal_check_aluminum,
+target.nsc_1 = source.nsc_1,
+target.nsc_2 = source.nsc_2,
+target.nsc_3 = source.nsc_3,
+target.nsc_4 = source.nsc_4,
+target.nsc_5 = source.nsc_5,
+target.nsc_6 = source.nsc_6,
+target.nsc_7 = source.nsc_7,
+target.nsc_8 = source.nsc_8,
+target.nsc_9 = source.nsc_9,
+target.nsc_10 = source.nsc_10
 
+WHEN NOT MATCHED THEN
+INSERT (
+base_product, 
+car_model, 
+product, 
+car_code, 
+block, 
+class, 
+line_no, 
+circuit_qty, 
+trd_nwpa_0_13, 
+trd_nwpa_below_2_0_except_0_13, 
+trd_nwpa_2_0_3_0, 
+trd_wpa_0_13, 
+trd_wpa_below_2_0_except_0_13, 
+trd_wpa_2_0_3_0, 
+tr327, 
+tr328, 
+trd_aluminum_nwpa_2_0, 
+trd_aluminum_nwpa_below_2_0, 
+trd_aluminum_wpa_2_0, 
+trd_aluminum_wpa_below_2_0, 
+aluminum_dimension_check_aluminum_terminal_inspection, 
+aluminum_visual_inspection, 
+aluminum_coating_uv_ii, 
+aluminum_image_inspection, 
+aluminum_uv_iii, 
+trd_alpha_aluminum_nwpa, 
+trd_alpha_aluminum_wpa, 
+aluminum_visual_inspection_for_alpha, 
+enlarged_terminal_check_for_alpha, 
+air_water_leak_test, 
+sam_sub_no_airbag, 
+sam_sub_no_normal, 
+jam_auto_crimping_and_twisting, 
+trd_alpha_aluminum_5_0_above, 
+point_marking_nsc, 
+point_marking_sam, 
+enlarged_terminal_check_aluminum, 
+nsc_1, 
+nsc_2, 
+nsc_3, 
+nsc_4, 
+nsc_5, 
+nsc_6, 
+nsc_7, 
+nsc_8, 
+nsc_9, 
+nsc_10)
+VALUES (
+source.base_product,
+source.car_model,
+source.product,
+source.car_code,
+source.block,
+source.class,
+source.line_no,
+source.circuit_qty,
+source.trd_nwpa_0_13,
+source.trd_nwpa_below_2_0_except_0_13,
+source.trd_nwpa_2_0_3_0,
+source.trd_wpa_0_13,
+source.trd_wpa_below_2_0_except_0_13,
+source.trd_wpa_2_0_3_0,
+source.tr327,
+source.tr328,
+source.trd_aluminum_nwpa_2_0,
+source.trd_aluminum_nwpa_below_2_0,
+source.trd_aluminum_wpa_2_0,
+source.trd_aluminum_wpa_below_2_0,
+source.aluminum_dimension_check_aluminum_terminal_inspection,
+source.aluminum_visual_inspection,
+source.aluminum_coating_uv_ii,
+source.aluminum_image_inspection,
+source.aluminum_uv_iii,
+source.trd_alpha_aluminum_nwpa,
+source.trd_alpha_aluminum_wpa,
+source.aluminum_visual_inspection_for_alpha,
+source.enlarged_terminal_check_for_alpha,
+source.air_water_leak_test,
+source.sam_sub_no_airbag,
+source.sam_sub_no_normal,
+source.jam_auto_crimping_and_twisting,
+source.trd_alpha_aluminum_5_0_above,
+source.point_marking_nsc,
+source.point_marking_sam,
+source.enlarged_terminal_check_aluminum,
+source.nsc_1,
+source.nsc_2,
+source.nsc_3,
+source.nsc_4,
+source.nsc_5,
+source.nsc_6,
+source.nsc_7,
+source.nsc_8,
+source.nsc_9,
+source.nsc_10
+);  ";
+$params = [
+$base_product,
+$car_model,
+$product,
+$car_code,
+$block,
+$class,
+$line_no,
+$circuit_qty,
+$trd_nwpa_0_13,
+$trd_nwpa_below_2_0_except_0_13,
+$trd_nwpa_2_0_3_0,
+$trd_wpa_0_13,
+$trd_wpa_below_2_0_except_0_13,
+$trd_wpa_2_0_3_0,
+$tr327,
+$tr328,
+$trd_aluminum_nwpa_2_0,
+$trd_aluminum_nwpa_below_2_0,
+$trd_aluminum_wpa_2_0,
+$trd_aluminum_wpa_below_2_0,
+$aluminum_dimension_check_aluminum_terminal_inspection,
+$aluminum_visual_inspection,
+$aluminum_coating_uv_ii,
+$aluminum_image_inspection,
+$aluminum_uv_iii,
+$trd_alpha_aluminum_nwpa,
+$trd_alpha_aluminum_wpa,
+$aluminum_visual_inspection_for_alpha,
+$enlarged_terminal_check_for_alpha,
+$air_water_leak_test,
+$sam_sub_no_airbag,
+$sam_sub_no_normal,
+$jam_auto_crimping_and_twisting,
+$trd_alpha_aluminum_5_0_above,
+$point_marking_nsc,
+$point_marking_sam,
+$enlarged_terminal_check_aluminum,
+$nsc_1,
+$nsc_2,
+$nsc_3,
+$nsc_4,
+$nsc_5,
+$nsc_6,
+$nsc_7,
+$nsc_8,
+$nsc_9,
+$nsc_10
 
-
-
-    WHERE car_model = ? 
-      AND product = ? 
-      AND car_code = ? 
-      AND block = ? 
-      AND class = ?
-END
-ELSE
-BEGIN
-    INSERT INTO first_process (
-        base_product,
-        car_model, product,
-        car_code,
-        block, class,
-        line_no,
-        circuit_qty, 
-        trd_nwpa_0_13,
-        trd_nwpa_below_2_0_except_0_13,
-        trd_nwpa_2_0_3_0, 
-        trd_wpa_0_13,
-        trd_wpa_below_2_0_except_0_13,
-        trd_wpa_2_0_3_0, 
-        tr327, tr328,
-        trd_aluminum_nwpa_2_0,
-        trd_aluminum_nwpa_below_2_0, 
-        trd_aluminum_wpa_2_0,
-        trd_aluminum_wpa_below_2_0,
-        aluminum_dimension_check_aluminum_terminal_inspection,
-        aluminum_visual_inspection,
-        aluminum_coating_uv_ii,
-        aluminum_image_inspection,
-        aluminum_uv_iii,
-        trd_alpha_aluminum_nwpa,
-        trd_alpha_aluminum_wpa,
-        aluminum_visual_inspection_for_alpha,
-        enlarged_terminal_check_for_alpha,
-        air_water_leak_test,
-        sam_sub_no_airbag,
-        sam_sub_no_normal,
-        jam_auto_crimping_and_twisting,
-        trd_alpha_aluminum_5_0_above,
-        point_marking_nsc,
-        point_marking_sam,
-        enlarged_terminal_check_aluminum,
-        nsc_1,
-        nsc_2,
-        nsc_3,
-        nsc_4,
-        nsc_5,
-        nsc_6,
-        nsc_7,
-        nsc_8,
-        nsc_9,
-        nsc_10
-
-        
-    )
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?,
-            ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,
-            ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,
-            ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,
-            ?, ?, ?, ?, ?, ?, ? )
-END
-
-        ";
-
-        $params = [
-            // Parameters for IF EXISTS condition
-            $car_model,
-            $product,
-            $car_code,
-            $block,
-            $class,
-
-            // Parameters for UPDATE operation
-            $base_product,
-            $line_no,
-            $circuit_qty,
-            $trd_nwpa_0_13,
-            $trd_nwpa_below_2_0_except_0_13,
-            $trd_nwpa_2_0_3_0,
-            $trd_wpa_0_13,
-            $trd_wpa_below_2_0_except_0_13,
-            $trd_wpa_2_0_3_0,
-            $tr327,
-            $tr328,
-            $trd_aluminum_nwpa_2_0,
-            $trd_aluminum_nwpa_below_2_0,
-            $trd_aluminum_wpa_2_0,
-            $trd_aluminum_wpa_below_2_0,
-            $aluminum_dimension_check_aluminum_terminal_inspection,
-            $aluminum_visual_inspection,
-            $aluminum_coating_uv_ii,
-            $aluminum_image_inspection,
-            $aluminum_uv_iii,
-            $trd_alpha_aluminum_nwpa,
-            $trd_alpha_aluminum_wpa,
-            $aluminum_visual_inspection_for_alpha,
-            $enlarged_terminal_check_for_alpha,
-            $air_water_leak_test,
-            $sam_sub_no_airbag,
-            $sam_sub_no_normal,
-            $jam_auto_crimping_and_twisting,
-            $trd_alpha_aluminum_5_0_above,
-            $point_marking_nsc,
-            $point_marking_sam,
-            $enlarged_terminal_check_aluminum,
-            $nsc_1,
-            $nsc_2,
-            $nsc_3,
-            $nsc_4,
-            $nsc_5,
-            $nsc_6,
-            $nsc_7,
-            $nsc_8,
-            $nsc_9,
-            $nsc_10,
-
-
-            // Parameters for WHERE clause of UPDATE operation
-            $car_model,
-            $product,
-            $car_code,
-            $block,
-            $class,
-
-            // Parameters for INSERT operation
-            $base_product,
-            $car_model,
-            $product,
-            $car_code,
-            $block,
-            $class,
-            $line_no,
-            $circuit_qty,
-            $trd_nwpa_0_13,
-            $trd_nwpa_below_2_0_except_0_13,
-            $trd_nwpa_2_0_3_0,
-            $trd_wpa_0_13,
-            $trd_wpa_below_2_0_except_0_13,
-            $trd_wpa_2_0_3_0,
-            $tr327,
-            $tr328,
-            $trd_aluminum_nwpa_2_0,
-            $trd_aluminum_nwpa_below_2_0,
-            $trd_aluminum_wpa_2_0,
-            $trd_aluminum_wpa_below_2_0,
-            $aluminum_dimension_check_aluminum_terminal_inspection,
-            $aluminum_visual_inspection,
-            $aluminum_coating_uv_ii,
-            $aluminum_image_inspection,
-            $aluminum_uv_iii,
-            $trd_alpha_aluminum_nwpa,
-            $trd_alpha_aluminum_wpa,
-            $aluminum_visual_inspection_for_alpha,
-            $enlarged_terminal_check_for_alpha,
-            $air_water_leak_test,
-            $sam_sub_no_airbag,
-            $sam_sub_no_normal,
-            $jam_auto_crimping_and_twisting,
-            $trd_alpha_aluminum_5_0_above,
-            $point_marking_nsc,
-            $point_marking_sam,
-            $enlarged_terminal_check_aluminum,
-            $nsc_1,
-            $nsc_2,
-            $nsc_3,
-            $nsc_4,
-            $nsc_5,
-            $nsc_6,
-            $nsc_7,
-            $nsc_8,
-            $nsc_9,
-            $nsc_10
         ];
 
-        $stmt = sqlsrv_prepare($conn, $sql, $params);
+       $stmt = sqlsrv_query($conn, $sql, $params);
         if ($stmt === false) {
-            $errors[] = 'Error preparing statement: ' . print_r(sqlsrv_errors(), true);
-            continue;
-        }
-
-        $result = sqlsrv_execute($stmt);
-        if ($result === false) {
             $errors[] = 'Error executing statement: ' . print_r(sqlsrv_errors(), true);
         } else {
             $processedCount++;
         }
+        
     }
 
     if (empty($errors)) {
@@ -335,3 +353,65 @@ END
 header('Content-Type: application/json');
 echo json_encode($response);
 ?>
+
+    
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
